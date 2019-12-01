dd7a0a0e31c42ec24e95cf60f18355f57545dc0b
Corina <corinacvp@gmail.com>
2019-12-01 17:53:18 -0300

    first changes in the DOM

--- src/data.js
+++ src/data.js
@@ -1,9 +1,30 @@
-/* Manejo de data */
+import LoL from './data/lol/lol.js'
 
-// esta es una función de ejemplo
+let contenedor = document.getElementById("contenedor");
+let backBtn = document.getElementById("backBtn");
 
+export const goToHome = () => {
+  contenedor.className="contenedor";
+  backBtn.className="hidden";
+  contenedor.innerHTML="";
+  contenedor.innerHTML=`<article class="titulo">CHOOSE CHAMPION</article>
+  <article class="texto"></article>
+  <div class="items" data-busqueda="name" data-imagen="data-imagen"></div>`;
+}
 
-/*export const img = () => {
-  return 'img';
+export const filterDataByRol = (rol) => {
+  
+  contenedor.innerHTML="";
+  contenedor.className="filterChamp";
+  backBtn.className="backBtn";
+
+  for (const i in LoL.data) {
+    contenedor.innerHTML +=
+     		 `<div class="elementos">
+     			<img class="img" src="${LoL.data[i].img}">
+          </div>`
+          for (const j in LoL.data[i].tags){
+            console.log(LoL.data[i].tags[j]);
+          }
+  }
 };
-*/
