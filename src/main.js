f9e710d4d775960754a2104e079021b884c2717e
rinarive <rinarive@gmail.com>
2019-12-02 09:28:38 -0300

    prueba

--- src/main.js
+++ src/main.js
@@ -1,12 +1,2 @@
 
-import LoL from './data/lol/lol.js'
-import { img } from './data.js';
-import { exportDefaultDeclaration } from '@babel/types';
-
-console.log(img);
-console.log(LoL);
-document.getElementById("figt").addEventListener("click", ()=>{
-
-document.getElementById("text").classList.add("../lol.js.data.imge");
-});
 
