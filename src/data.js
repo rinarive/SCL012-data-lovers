b91308700058d4aac13d8f8c3c65dcf8896773e3
Corina <corinacvp@gmail.com>
2019-12-05 13:54:13 -0300

    function sort by A-Z

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ import LoL from './data/lol/lol.js'
 let contenedor = document.getElementById("elemento");
 let backBtn = document.getElementById("backBtn");
 
-//Creamos un array vacio para llenarlo de data de los campeones (obj type array)
+//Array vacio para llenarlo de data de LoL (obj type array)
 let arrayChampions = [];
 
 //Llenamos el array con la data de LoL, para tratarlo como arreglo y nos reconozca la funcion filter
@@ -12,28 +12,26 @@ for (const i in LoL.data) {
 
 // Funcion para filtrar por Rol 
 export const filterDataByRol = (rol) => {
-
   contenedor.innerHTML = "";
   contenedor.className = "filterChamp"; // Style contenedor tarjetas
-  backBtn.className = "backBtn"; // Style para que este visible 
-
-  // funcion para filtrar
+  // backBtn.className = "backBtn"; // Style para que este visible 
+  // Funcion para filtrar
   let result = arrayChampions.filter(element => { // Element= cada vuelta del bucle
     return element.tags.includes(rol);
   });
   // Arreglo con los campeones filtrados por rol 
   result.forEach(element => {
-
     contenedor.innerHTML +=
       `<div class="elementos">
-    <img class="img" src="${element.splash}">
-    <h1 class="name">${element.name}</h1>
-    <p class"titulo">${element.title}</p>
-    </div>`
+         <img class="img" src="${element.splash}">
+         <h1 class="name">${element.name}</h1>
+         <p class"titulo">${element.title}</p>
+         <p class"titulo">${element.tags}</p>
+       </div>`
   });
 };
 
-//funcion para ir al menu principal
+//Funcion para ir al menu principal
 export const goToHome = () => {
   contenedor.className = "elemento";
   backBtn.className = "hidden";
